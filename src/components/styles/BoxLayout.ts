93f63529b3adf6c5f241b921e261e1cd4aaf9267
Daniel Vitor Madeira Reis <147751321+Daniel-Vitor-infnet@users.noreply.github.com>
2025-02-23 03:58:04 -0300

    Ajustes

---
+++ src/components/styles/BoxLayout.ts
@@ -0,0 +1,13 @@
+const BoxLayout = {
+    width: '80vw',
+    minHeight: '80vh',
+    border: '1px solid red',
+    marginTop: '2vh',
+    backgroundColor: 'seagreen',
+    mx: 'auto', // Centraliza horizontalmente
+    borderRadius: 2,
+    boxShadow: 2,
+  };
+  
+  export default BoxLayout;
+  
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Container,  Typography, Card, CardMedia, CardContent, CardActions, Button } from '@mui/material';
 import { Grid, Box } from '../components';
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
 import HeaderPers from '../components/elementos/header';
 import SlidePers from '../components/elementos/home/slide';
-import { Margin } from '@mui/icons-material';
+import BoxLayout from '../components/styles/BoxLayout';
 
 
 
@@ -13,27 +11,15 @@ import { Margin } from '@mui/icons-material';
 const Home: React.FC = () => {
 
 
-  const containerStyles = {
-    width: '70vw',
-    minHeight: '100vh',
-    border: '1px solid black',
-    marginTop: '4vh',
-    backgroundColor: 'mediumspringgreen',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center', 
-    mx: 'auto', 
-    borderRadius: 2,
-    boxShadow: 2,
-    p: 2
-  };
 
   return (
     <>
       <HeaderPers />
 
-      <Box >
-        <p>Teste</p>
+      <Box sx={BoxLayout}>
+        <Grid>
+           <p>Teste</p>
+        </Grid>
       </Box>
 
       {/* <SlidePers /> */}
