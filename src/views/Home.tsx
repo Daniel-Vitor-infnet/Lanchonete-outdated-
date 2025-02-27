51ff91e7931657679a2e9bce79117aae5fdbea33
Daniel Vitor Madeira Reis <147751321+Daniel-Vitor-infnet@users.noreply.github.com>
2025-02-27 05:40:55 -0300

    Update Home.tsx

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -8,7 +8,6 @@ import slide from '../assets/img/logo.png';
 import logo1 from '../assets/img/logo1.png';
 import slide2 from '../assets/img/teste.png';
 import slide3 from '../assets/img/teste3.jpg';
-import { Padding } from '@mui/icons-material';
 
 
 
@@ -29,9 +28,10 @@ const Home: React.FC = () => {
     height: '160px',
     position: 'absolute',
     zIndex: '1',
-    top: '30%',
-    left: '44%',
-    outline: '4px solid red',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -130%)',
+    outline: '4px solid skyblue',
   }
 
   const avatarStilos = {
@@ -39,6 +39,20 @@ const Home: React.FC = () => {
     height: '100%',
     outline: '4px solid darkorchid',
   }
+
+  const pStilos: React.CSSProperties = {
+    position: 'absolute',
+    fontSize: '0.8rem',
+    padding: '1.8px',
+    borderRadius: '6px',
+    zIndex: 2,
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, 380%)',
+    color: 'white',
+    backgroundColor: 'red',
+  };
+  
 
 
   return (
@@ -51,6 +65,7 @@ const Home: React.FC = () => {
         </Grid>
         <Grid sx={gridAvatarStilos}>
           <Avatar sx={avatarStilos} alt="Logo" src={logo1} />
+          <p style={pStilos}>FECHADO</p>
         </Grid>
 
       </BoxLayout>
