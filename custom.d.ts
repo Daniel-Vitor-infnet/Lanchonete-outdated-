0d86ade025f555529fe2636334132a29ff315ee1
Daniel Vitor Madeira Reis <147751321+Daniel-Vitor-infnet@users.noreply.github.com>
2025-02-27 19:10:51 -0300

    update

--- custom.d.ts
+++ custom.d.ts
@@ -19,3 +19,8 @@ declare module "*.svg" {
     const value: string;
     export default value;
 }
+
+declare module '*.scss' {
+    const content: { [className: string]: string };
+    export default content;
+}
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Typography, Card, CardMedia, CardContent, CardActions, Button } from '@mui/material';
+import styles from '../styles/Home.module.scss';
 import { Grid, Box, Avatar } from '../components';
 import HeaderPers from '../components/elementos/header';
 import ImageCarousel from '../components/elementos/home/slide.tsx';
@@ -10,72 +10,22 @@ import slide2 from '../assets/img/teste.png';
 import slide3 from '../assets/img/teste3.jpg';
 import SimpleCard from '../components/elementos/home/cards.tsx';
 
-
-
-
 const Home: React.FC = () => {
-
-
-  const gridStylesCarrocel = {
-    height: '25vh',
-    backgroundColor: 'mediumspringgreen',
-    mx: 'auto', // Centraliza horizontalmente
-    overflow: 'hidden',
-  };
-
-  const gridAvatarStyles = {
-    width: '160px',
-    height: '160px',
-    position: 'relative',
-    zIndex: '1',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    outline: '4px solid skyblue',
-  }
-
-  const avatarStyles = {
-    width: '100%',
-    height: '100%',
-    outline: '4px solid darkorchid',
-  }
-
-  const statusStyles: React.CSSProperties = {
-    position: 'absolute',
-    fontSize: '0.8rem',
-    padding: '1.8px',
-    borderRadius: '6px',
-    zIndex: 2,
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, 380%)',
-    color: 'white',
-    backgroundColor: 'red',
-  };
-
-
-  const gridStylesCards = {
-    backgroundColor: 'blue',
-  };
-  
-
-
   return (
     <>
       <HeaderPers />
 
       <BoxLayout>
-        <Grid sx={gridStylesCarrocel}>
+        <Grid className={styles.carousel}>
           <ImageCarousel images={[slide, slide2]} />
         </Grid>
-        <Grid sx={gridAvatarStyles}>
-          <Avatar sx={avatarStyles} alt="Logo" src={logo1} />
-          <p style={statusStyles}>FECHADO</p>
+        <Grid className={styles['avatar-container']}>
+          <Avatar className={styles.avatar} alt="Logo" src={logo1} />
+          <p className={styles.status}>FECHADO</p>
         </Grid>
-        <Grid sx={gridStylesCards}>
+        <Grid className={styles.cards}>
           <SimpleCard />
         </Grid>
-
       </BoxLayout>
     </>
   );
