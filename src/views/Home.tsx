12bd1d022b8503f0e705093f13b7e0c6807f0750
Daniel Vitor Madeira Reis <147751321+Daniel-Vitor-infnet@users.noreply.github.com>
2025-03-01 04:11:25 -0300

    up

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import styles from '../styles/Home.module.scss';
 import { Grid2, Box, Avatar } from '@mui/material';
-import HeaderPers from '../components/elementos/header';
+import HeaderPers from '../components/elementos/header.tsx';
+import FooterPers from '../components/elementos/footer.tsx';
 import ImageCarousel from '../components/elementos/home/slide.tsx';
 import BoxLayout from '../components/layout/BoxLayout.tsx';
 import slide from '../assets/img/logo.png';
-import logo1 from '../assets/img/logo1.png';
-import slide2 from '../assets/img/teste.png';
-import slide3 from '../assets/img/teste3.jpg';
-import SimpleCard from '../components/elementos/home/cards.tsx';
+import logo from '../assets/img/Logo.png';
+import slide2 from '../assets/img/Sorvete.png';
+import SimpleCard from '../components/elementos/cards.tsx';
 
 const Home: React.FC = () => {
   return (
@@ -20,11 +20,12 @@ const Home: React.FC = () => {
           <ImageCarousel images={[slide, slide2]} />
         </Grid2>
         <Grid2 className={styles['avatar-container']}>
-          <Avatar className={styles.avatar} alt="Logo" src={logo1} />
+          <Avatar className={styles.avatar} alt="Logo" src={logo} />
           <p className={styles.status}>FECHADO</p>
         </Grid2>
         <SimpleCard />
       </BoxLayout>
+      <FooterPers />
     </Grid2>
   );
 };
