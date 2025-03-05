040510436e1257dd1d927cec1c61a15d525a92fa
Daniel Vitor Madeira Reis <147751321+Daniel-Vitor-infnet@users.noreply.github.com>
2025-03-05 09:23:04 -0300

    up

--- src/views/Panel.tsx
+++ src/views/Panel.tsx
@@ -4,8 +4,7 @@ import { alpha, styled } from '@mui/material/styles';
 import { pink } from '@mui/material/colors';
 import Switch from '@mui/material/Switch';
 import { AlertDiagPers, ButtonOnOff, TimeSelectPerso } from '@/components';
-import { jaJP } from '@mui/x-date-pickers/locales';
-
+import styles from '@/styles/Panel.module.scss'; // Importando CSS Module
 
 const PinkSwitch = styled(Switch)(({ theme }) => ({
   '& .MuiSwitch-switchBase.Mui-checked': {
@@ -21,93 +20,36 @@ const PinkSwitch = styled(Switch)(({ theme }) => ({
 
 const label = { inputProps: { 'aria-label': 'Color switch demo' } };
 
-
-
 const Panel: React.FC = () => {
-
-
-
-
-  const GridStyke = {
-    display: "flex",
-    height: "80vh",
-    justifyContent: "center",
-    alignItems: "center",
-  }
-
-  const BoxStyle = {
-    backgroundColor: "white",
-    minWidth: "40%",
-    borderRadius: "10px",
-    padding: "60px",
-  }
-
-  const TypoTitleStyles = {
-    fontSize: "24px",
-    fontWeight: "bold",
-    marginBottom: "20px",
-    display: "flex",
-    justifyContent: "center",
-  }
-
-  const TypoSubTitleStyles = {
-    display: "flex",
-    alignItems: "center",
-  }
-
-  const GridOptStyke = {
-    backgroundColor: "white",
-    display: "flex",
-    justifyContent: "space-between",
-    borderBottom: "1px solid #000000",
-    padding: "20px",
-  }
-
-
-  const GridButtonStyles = {
-    display: "flex",
-    justifyContent: "end",
-    marginTop: "40px",
-  }
-
-  const ButtonStyles = {
-    backgroundColor: "dodgerblue",
-    color: "white",
-    padding: "7px",
-  }
-
-
   return (
-    <Grid2 style={GridStyke}>
-      {/* <AlertDiagPers message='As opções ainda não estão funcionando' /> */}
-      <Box style={BoxStyle}>
-        <Typography style={TypoTitleStyles}>
+    <Grid2 className={styles.mainContainer}>
+      <AlertDiagPers message="As opções ainda não estão funcionando" />
+      <Box className={styles.box}>
+        <Typography className={styles.title}>
           Painel de configurações
         </Typography>
-        <Grid2 style={GridOptStyke}>
-          <Typography sx={TypoSubTitleStyles}> Estabelecimento </Typography>
-          <ButtonOnOff optLeft='Fechar' optRight='Abrir' />
+        <Grid2 className={styles.optionContainer}>
+          <Typography className={styles.subTitle}>Estabelecimento</Typography>
+          <ButtonOnOff optLeft="Fechar" optRight="Abrir" />
         </Grid2>
-        <Grid2 style={GridOptStyke}>
-          <Typography sx={TypoSubTitleStyles}> Teste 1 </Typography>
+        <Grid2 className={styles.optionContainer}>
+          <Typography className={styles.subTitle}>Teste 1</Typography>
           <PinkSwitch {...label} defaultChecked />
         </Grid2>
-        <Grid2 style={GridOptStyke}>
-          <Typography sx={TypoSubTitleStyles}> Teste 2 </Typography>
+        <Grid2 className={styles.optionContainer}>
+          <Typography className={styles.subTitle}>Teste 2</Typography>
           <PinkSwitch {...label} defaultChecked />
         </Grid2>
-        <Grid2 style={GridOptStyke}>
-          <Typography sx={TypoSubTitleStyles}> Teste 3 </Typography>
+        <Grid2 className={styles.optionContainer}>
+          <Typography className={styles.subTitle}>Tempo De Espera</Typography>
           <TimeSelectPerso />
         </Grid2>
-        <Grid2 style={GridOptStyke}>
-          <Typography sx={TypoSubTitleStyles}> Teste 4 </Typography>
+        <Grid2 className={styles.optionContainer}>
+          <Typography className={styles.subTitle}>Teste 4</Typography>
           <ButtonOnOff />
         </Grid2>
-        <Grid2 sx={GridButtonStyles}>
-          <Button sx={ButtonStyles}>
-            Aplicar
-          </Button>
+        <Grid2 className={styles.buttonContainer}>
+          <Button className={styles.button}>Aplicar</Button>
         </Grid2>
       </Box>
     </Grid2>
