c78deaa341e24a9db80263df0147e27bf4de3cd9
Daniel Vitor Madeira Reis <147751321+Daniel-Vitor-infnet@users.noreply.github.com>
2025-03-10 20:57:38 -0300

    Ajustes do botão aplicar

--- src/views/Panel.tsx
+++ src/views/Panel.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import styles from '@/styles/Panel.module.scss';
 import { Grid2, Box, Typography, Button, Switch, pink, alpha, styled } from '@/libs/mui';
-import { AlertDiagPers, ButtonOnOff, TimeSelectPerso, ButtonPerson } from '@/components';
+import { AlertDiagPers, ButtonOnOff, TimeSelectPerso, ButtonPerson, mixins } from '@/components';
 import { Status, StatusMobile } from '@/views/subElementos/panel/status';
 
 const mediaQuery = window.matchMedia("(min-width: 1024px)");
@@ -21,10 +21,30 @@ const PinkSwitch = styled(Switch)(({ theme }) => ({
 
 const label = { inputProps: { 'aria-label': 'Color switch demo' } };
 
-const buttonStyles = ({
+const buttonPedidosCardsStyles = ({
   background: 'linear-gradient(45deg, #FF1044 30%, #FF8E53 90%)',
   with: '100%',
   height: '100%',
+  textTransform: "uppercase",
+});
+
+const buttonAplicarStyles = ({
+  background: 'blue',
+  height: '35px',
+  width: '160px',
+  "&:hover": {
+    background: "linear-gradient(135deg, oklch(0.59 0.22 261.41), oklch(0.59 0.22 261.37))", // Gradiente mais escuro no hover
+    boxShadow: "unset",
+    transform: "unset", // Levanta levemente o botão
+  },
+  "&:active": {},
+
+  "&::before": {},
+  "&:hover::before": {},
+  [mixins.laptop]: {
+    height: '43px',
+    width: '140px',
+  },
 });
 
 
@@ -56,13 +76,13 @@ const Panel: React.FC = () => {
           <Typography className={styles.subTitle}>Teste 4</Typography>
           <ButtonOnOff />
         </Grid2>
-        <Grid2 className={styles.buttonContainer}>
-          <Button className={styles.button}>Aplicar</Button>
+        <Grid2 className={styles.buttonAplicarContainer}>
+          <ButtonPerson text='Aplicar' customStyles={buttonAplicarStyles} />
         </Grid2>
       </Box>
-      <Grid2 className={styles.buttonPedidosContainer}>
-        <ButtonPerson text='Ver Pediodos' customStyles={buttonStyles}/>
-        <ButtonPerson text='Editar Itens' customStyles={buttonStyles}/>
+      <Grid2 className={styles.buttonPedidosCardsContainer}>
+        <ButtonPerson text='Ver Pediodos' customStyles={buttonPedidosCardsStyles} />
+        <ButtonPerson text='Editar Itens' customStyles={buttonPedidosCardsStyles} />
       </Grid2>
     </Grid2>
   );
