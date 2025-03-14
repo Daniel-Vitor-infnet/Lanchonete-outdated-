1163abf2ada1c1964bb93026b6b845c1f121c6bf
Daniel Vitor Madeira Reis <147751321+Daniel-Vitor-infnet@users.noreply.github.com>
2025-03-14 08:18:48 -0300

    Update cardsCategoriaTemp.json
    Fixed Multi-Screen
    
    Ajuste para multi screen e correções do cardapio. Estilos ajustados

--- src/views/Cardapio.tsx
+++ src/views/Cardapio.tsx
@@ -4,6 +4,7 @@ import cardsCategoriaDataJson from "@/utils/cardsCategoriaTemp.json";
 import cardsItensDataJson from "@/utils/cardsItemTemp.json";
 import { Card, CardContent, Typography, Grid2, Tab, Box, Tabs } from "@/libs/mui";
 import { mixins, CardCategoria } from '@/components';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 const a11yProps = (index: number) => {
   return {
@@ -13,17 +14,45 @@ const a11yProps = (index: number) => {
 };
 
 const mainContainer = {
-  backgroundColor: "red",
+  width: "100vw !important",
+  height: "min(87vh, 87dvh) !important",
+  display: "flex",
+  justifyContent: 'center',
+  alignItems: 'center',
 };
 
-
 const boxPrincipal = {
-  width: "min(100vh, 100dvh)",
-  height: "30vw",
-  flexGrow: 1,
-  bgcolor: 'background.paper',
+  width: "90vw",
+  maxWidth: "90vw",
+  height: "min(80vh, 80dvh)",
+  maxHeight: "min(80vh, 80dvh)",
+  backgroundColor: "white",
   display: 'flex',
 }
+
+const barraLateralContainer = {
+  borderRight: 5,
+  borderColor: 'divider',
+  backgroundColor: 'orange',
+  display: 'flex',
+  flexShrink: 0,
+}
+
+const barraLateral = {
+  display: 'flex',
+  gap: 1,
+  width: '100%',
+}
+
+const cardContainer = {
+  backgroundColor: 'purple',
+  width: '97%',
+  maxWidth: '97%',
+  maxHeight: "94%",
+  margin: 3,
+  overflowX: 'auto',
+}
+
 
 const VerticalTabs: React.FC = () => {
   const [value, setValue] = React.useState<number>(0);
@@ -41,19 +70,32 @@ const VerticalTabs: React.FC = () => {
           value={value}
           onChange={handleChange}
           aria-label="Vertical tabs example"
-          sx={{ borderRight: 1, borderColor: 'divider' }}
+          sx={barraLateralContainer}
         >
           {cardsCategoriaDataJson.map((card: any, index: number) => (
-            <Tab key={index} label={card.title} {...a11yProps(index)} />
+            <Tab
+              key={index}
+              label={(
+                <Box sx={barraLateral}>
+                  <WhatsAppIcon fontSize="small" />
+                  {card.title}
+                </Box>
+              )}
+              {...a11yProps(index)}
+            />
           ))}
         </Tabs>
 
-        <Grid2 sx={{ p: 3 }}>
+        <Grid2 >
           {cardsCategoriaDataJson.map((item: any, index: number) => {
             if (value === index) {
               const itensDaCategoria = cardsItensDataJson.find(card => card.category === item.title)?.items || null;
               if (itensDaCategoria) {
-                return <CardCategoria key={index} cardsItens={itensDaCategoria} stylesPerso={stylesCardCategoria} />;
+                return (
+                  <Grid2 sx={cardContainer}>
+                    <CardCategoria key={index} cardsItens={itensDaCategoria} stylesPerso={stylesCardCategoria} />
+                  </Grid2>
+                )
               } else {
                 return (
                   <Typography>Nenhum item desse categoria</Typography>
