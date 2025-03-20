5c3f6b665bd6827446710a63c2f1fb1153ff8421
Daniel Vitor Madeira Reis <147751321+Daniel-Vitor-infnet@users.noreply.github.com>
2025-03-20 07:50:22 -0300

    Update iconsSelect.tsx

--- src/utils/function/iconsSelect.tsx
+++ src/utils/function/iconsSelect.tsx
@@ -7,7 +7,6 @@ import iconesMdi from '@/libs/mdi';
 export default function iconSelect(icon: any, tamanho: any) {
 
     const tamanhoMap: Record<string, [number, string]> = {
-        muitoPequeno: [0.8, "P"],
         pequeno: [1, "P"],
         medio: [2, "M"],
         grande: [3, "G"],
@@ -15,9 +14,12 @@ export default function iconSelect(icon: any, tamanho: any) {
 
     const [libs, categoria, item]: [string, string, string] = icon.split("-", 3) as [string, string, string];
 
+    const tamanhoPerso = typeof tamanho === "number" ? tamanho : tamanhoMap[tamanho][0];
+
+
     switch (libs) {
         case "mdi":
-            return <IconMdi path={iconesMdi[categoria][item]} size={tamanhoMap[tamanho][0]} />
+            return <IconMdi path={iconesMdi[categoria][item]} size={tamanhoPerso} />
         case 'mui':
             return null;
         default:
