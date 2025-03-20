3b46be1c3d23bd6ecfcecf06ae5a7cc78242c492
Daniel Vitor Madeira Reis <147751321+Daniel-Vitor-infnet@users.noreply.github.com>
2025-03-20 00:57:18 -0300

    Fixed Screen Desktop

--- src/components/layout/cardapio/cardsCardapio.tsx
+++ src/components/layout/cardapio/cardsCardapio.tsx
@@ -1,22 +1,24 @@
 import React from "react";
 import { Card, CardContent, Typography, Grid2 } from "@/libs/mui";
-import { redimencionarTexto } from "@/utils/function/cardapio";
+import { obterTamanhoTela } from "@/utils/function";
 
 interface CardsListProps {
   cardsCardapio: any;
   stylesPerso: any;
 }
 
+
 const CardsList: React.FC<CardsListProps> = ({ cardsCardapio, stylesPerso }) => {
   return (
     <Grid2 className={stylesPerso['main-container']}>
       {cardsCardapio.map((item: any) => {
+        const titleTamanho = item.title.length > obterTamanhoTela(11) ? 'item-title-grande' : 'item-title-pequeno';
         return (
           <Grid2 key={item.id}>
             <Card className={stylesPerso['item']} key={item.id}>
               <img src={item.image} alt={item.title} className={stylesPerso['item-image']} />
               <CardContent sx={{ p: 0 }} className={stylesPerso['item-info']}>
-                <Typography className={stylesPerso['item-title']}>
+                <Typography className={stylesPerso[titleTamanho]}>
                   {item.title}
                 </Typography>
                 <Typography className={stylesPerso['item-description']}>
