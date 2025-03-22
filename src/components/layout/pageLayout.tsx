9c7dc162d1c8984a2018381af42579f107264dda
Daniel Vitor Madeira Reis <147751321+Daniel-Vitor-infnet@users.noreply.github.com>
2025-03-21 21:57:00 -0300

    Create Header
    Criado Header novo

--- src/components/layout/pageLayout.tsx
+++ src/components/layout/pageLayout.tsx
@@ -1,27 +1,18 @@
 import React from "react";
 import { Grid2 } from "@/libs/mui";
-import HeaderPers from "@/components/elementos/header";
+import HeaderPers from "@/components/elementos/Header1";
 import FooterPers from "@/components/elementos/footer";
 import { useAppContext } from "@/Context";
 
-const layoutStyles = {
-  display: "flex",
-  flexDirection: "column",
-  minHeight: "min(100dvh, 100vh)",
-  overflowX: "hidden",
-};
 
-const contPrincipalStyles = {
-  flex: 1,
-};
 
 const PageLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const { hideFooter } = useAppContext();
 
   return (
-    <Grid2 sx={layoutStyles}>
+    <Grid2 >
       <HeaderPers />
-      <Grid2 sx={contPrincipalStyles}>
+      <Grid2 >
         {children}
       </Grid2>
       {!hideFooter && <FooterPers />}
