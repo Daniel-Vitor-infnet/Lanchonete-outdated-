ef0ae367c582a32a70311e4c3315c22511c11220
Daniel Vitor Madeira Reis <147751321+Daniel-Vitor-infnet@users.noreply.github.com>
2025-03-22 01:08:49 -0300

    Add Icons MUI

--- src/libs/mui/icons.ts
+++ src/libs/mui/icons.ts
@@ -1,45 +1,11 @@
-//Imports de comida
-import { mdiBaguette } from '@mdi/js';
-import { mdiBeer } from '@mdi/js';
-import { mdiBottleSodaClassic } from '@mdi/js';
-import { mdiCakeVariant } from '@mdi/js';
-import { mdiCoffee } from '@mdi/js';
-import { mdiFood } from '@mdi/js';
-import { mdiFoodDrumstick } from '@mdi/js';
-import { mdiFoodHotDog } from '@mdi/js';
-import { mdiFoodSteak } from '@mdi/js';
-import { mdiFrenchFries } from '@mdi/js';
-import { mdiGlassMugVariant } from '@mdi/js';
-import { mdiHamburger } from '@mdi/js';
-import { mdiIcePop } from '@mdi/js';
-import { mdiPizza } from '@mdi/js';
-import { mdiSilverware } from '@mdi/js';
-import { mdiSilverwareForkKnife } from '@mdi/js';
-import { mdiSilverwareVariant } from '@mdi/js';
-import { mdiWater } from '@mdi/js';
+//Imports Geral
+import muiAccountCircle from '@mui/icons-material/AccountCircle';
 
 //Imports de rede social
 
-const icones: Record<string, Record<string, string>> = {
-    comida: {
-        pao: mdiBaguette,
-        copoCheio: mdiBeer,
-        garrafa: mdiBottleSodaClassic,
-        bolo: mdiCakeVariant,
-        cafe: mdiCoffee,
-        fastFood: mdiFood,
-        frango: mdiFoodDrumstick,
-        hotDog: mdiFoodHotDog,
-        carne: mdiFoodSteak,
-        batataFrita: mdiFrenchFries,
-        cerveja: mdiGlassMugVariant,
-        hamburguer: mdiHamburger,
-        picole: mdiIcePop,
-        pizza: mdiPizza,
-        talheres: mdiSilverware,
-        talheres2: mdiSilverwareForkKnife,
-        talheres3: mdiSilverwareVariant,
-        gotaDeAgua: mdiWater
+const icones: Record<string, Record<string, any>> = {
+    geral: {
+        AccountCircle: muiAccountCircle
     }
 
 };
--- src/utils/function/iconsSelect.tsx
+++ src/utils/function/iconsSelect.tsx
@@ -1,27 +1,25 @@
 import IconMdi from '@mdi/react';
 import iconesMdi from '@/libs/mdi';
+import iconesMUI from '@/libs/mui/icons';
 
 
 
 
-export default function iconSelect(icon: any, tamanho: any) {
+export default function iconSelect(icon: string, tamanho?: number, cor?: string) {
 
-    const tamanhoMap: Record<string, [number, string]> = {
-        pequeno: [1, "P"],
-        medio: [2, "M"],
-        grande: [3, "G"],
-      };
+
 
     const [libs, categoria, item]: [string, string, string] = icon.split("-", 3) as [string, string, string];
 
-    const tamanhoPerso = typeof tamanho === "number" ? tamanho : tamanhoMap[tamanho][0];
-
+    let IconeEscolhido = null;
 
     switch (libs) {
         case "mdi":
-            return <IconMdi path={iconesMdi[categoria][item]} size={tamanhoPerso} />
+            IconeEscolhido = iconesMdi[categoria][item];
+            return <IconMdi path={IconeEscolhido} size={tamanho} />
         case 'mui':
-            return null;
+            IconeEscolhido = iconesMUI[categoria][item];
+            return <IconeEscolhido sx={{ fontSize: String(tamanho) || "1.0", color: cor || "#666666" }}/>
         default:
             return null;
     }
