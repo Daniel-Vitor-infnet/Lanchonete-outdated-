e42f2aa2d69abc82c8a89ffb5797c6a5628b8175
Daniel Vitor Madeira Reis <147751321+Daniel-Vitor-infnet@users.noreply.github.com>
2025-03-23 07:32:28 -0300

    Footer Mobile

--- src/components/elementos/Footer.tsx
+++ src/components/elementos/Footer.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Box, Typography, IconButton, Grid2 } from '@/libs/mui';
 import styles from "@/styles/pageLayout/Footer.module.scss";
-import { iconsSelect } from '@/utils/function';
+import { obterTamanhoTela, iconsSelect } from "@/utils/function";
+
 
 const Footer: React.FC = () => {
+    const tamanhoIcon = obterTamanhoTela(1.7, 1.3);
   return (
     <Box className={styles['footer-container']}>
       {/* Nome da empresa */}
@@ -15,26 +17,28 @@ const Footer: React.FC = () => {
       <Grid2 className={styles['grid-container']}>
         <Grid2 className={styles['contact-container']}>
           <Typography className={styles['icon-text']}>
-            {iconsSelect("mui-geral-Phone", null, "#4caf50")} 
+            {iconsSelect("mui-geral-Phone", tamanhoIcon, "#4caf50")} 
             +55 11 99999-9999
-            {iconsSelect("mui-geral-LocationOn", null, "#ff5722")} 
+            {iconsSelect("mui-geral-LocationOn", tamanhoIcon, "#ff5722")} 
             Rua Exemplo, 123 - Cidade
-            {iconsSelect("mui-geral-Email", null, "#ffcc00")} 
+            {iconsSelect("mui-geral-Email", tamanhoIcon, "#ffcc00")} 
             exemplo@gmail.com
           </Typography>
         </Grid2>
       </Grid2>
 
+      
+
       {/* Redes Sociais */}
       <Box className={styles['social-media-container']}>
         <IconButton href="https://facebook.com" target="_blank">
-          {iconsSelect("mui-geral-Facebook", null, "#1877F2")}
+          {iconsSelect("mui-geral-Facebook", tamanhoIcon, "#1877F2")}
         </IconButton>
         <IconButton href="https://instagram.com" target="_blank">
-          {iconsSelect("mui-geral-Instagram", null, "#E4405F")}
+          {iconsSelect("mui-geral-Instagram", tamanhoIcon, "#E4405F")}
         </IconButton>
         <IconButton href="https://wa.me/5511999999999" target="_blank">
-          {iconsSelect("mui-geral-WhatsApp", null, "#25D366")}
+          {iconsSelect("mui-geral-WhatsApp", tamanhoIcon, "#25D366")}
         </IconButton>
       </Box>
 
